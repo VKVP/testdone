a310d3837bc6145ad65631c12631738121ef85bd
VKVP <vamsikrishnamalagaveli40@gmail.com>
2024-12-14 16:38:05 +0530

    adding code

--- testpython.py
+++ testpython.py
@@ -1,1 +1,26 @@
-print('hello world')
+import boto3
+s3obj = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('vamsi_emp')
+
+def lambda_handler(event,context):
+    bucket_name = 'vamsource'
+    file_name = 'emp.csv'
+    resp = s3obj.get_object(Bucket = bucket_name, Key = file_name)
+    data = resp['Body'].read().decode('utf-8')
+    employees=data.split('\n')
+    print(employees)
+    
+    for emp in employees:
+        emp_id,emp_name,emp_prof = emp.split(',')
+        #adding print function
+        try:
+            response = table.put_item(
+                Item = {
+                    'emp_id':emp_id,
+                    'emp_name':emp_name,
+                    'emp_prof':emp_prof
+                }
+                )
+        except Exception as e:
+            raise e
